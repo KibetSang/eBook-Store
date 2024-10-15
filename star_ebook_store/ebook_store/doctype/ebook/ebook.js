197849df0dca7b033da6c56cac7235e59df07c61
Kibet Sang <kibetsang95@gmail.com>
2024-10-15 16:22:37 +0300

    Master

---
+++ star_ebook_store/ebook_store/doctype/ebook/ebook.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Kibet and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("eBook", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ star_ebook_store/ebook_store/doctype/ebook/ebook.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Kibet and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.website.website_generator import WebsiteGenerator
+
+
+class eBook(WebsiteGenerator):
+	pass
